20ac145a5dc7c8e2a474dffea5d689ec248361f4
Oari Kirian <versease101@gmail.com>
2017-06-10 22:01:50 +0800

    finished news page setup

---
+++ application/views/pages/news.php
@@ -0,0 +1,34 @@
+<div class="container-fluid">
+  <h2><span class="fa fa-bell"></span> Check Out The Latest News <small>Lorem ipsum dolor sit amet</small></h2>
+</div>
+
+<hr>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="list-group text-center">
+        <a href="#" class="list-group-item">
+          <h4 class="list-group-item-heading"><span class="fa fa-institution"></span> Campus News</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </a>
+        <a href="#" class="list-group-item">
+          <h4 class="list-group-item-heading"><span class="fa fa-briefcase"></span> Business</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </a>
+        <a href="#" class="list-group-item">
+          <h4 class="list-group-item-heading"><span class="fa fa-microchip"></span> Technology</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </a>
+        <a href="#" class="list-group-item">
+          <h4 class="list-group-item-heading"><span class="fa fa-soccer-ball"></span> Sports</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </a>
+        <a href="#" class="list-group-item">
+          <h4 class="list-group-item-heading"><span class="fa fa-pencil"></span> Literary</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
