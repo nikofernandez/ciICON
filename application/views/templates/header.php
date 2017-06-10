5c9381f2021d03885ed2c9bf2363ebba7426c9ca
Oari Kirian <versease101@gmail.com>
2017-06-10 22:26:48 +0800

    changed theme to Flatly and override navbar css to custom css and added new links (Contact Us, Submit Photo, Submit Article, Join the Team)

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -15,11 +15,17 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
     <!-- Bootswatch CDN -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/paper/bootstrap.min.css" rel="stylesheet" integrity="sha384-awusxf8AUojygHf2+joICySzB780jVvQaVCAt1clU3QsyAitLGul28Qxb2r1e5g+" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css" rel="stylesheet" integrity="sha384-+ENW/yibaokMnme+vBLnHMphUYxHs34h9lpdbSLuAwGkOKFRl4C34WkjazBtb7eT" crossorigin="anonymous">
 
     <style media="screen">
-      .navbar, .navbar-inverse{
+      .navbar, .navbar-inverse, .navbar-default a{
         background-color: #F4D03F !important;
+        color: #000 !important;
+      }
+
+      .navbar-default a:hover{
+        background-color: #17202A !important;
+        color: #FDFEFE !important;
       }
 
       .box{
