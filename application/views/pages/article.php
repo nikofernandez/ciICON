7dc1ff842a08f09f894386199e31658a5da8d015
Oari Kirian <versease101@gmail.com>
2017-06-10 22:51:12 +0800

    finished article page setup

---
+++ application/views/pages/article.php
@@ -0,0 +1,64 @@
+<div class="container-fluid">
+  <h2><span class="fa fa-newspaper-o"></span> Got Any Articles To Share?</h2>
+</div>
+
+<hr>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-6">
+      <form class="well">
+        <div class="form-group">
+          <label for="name">Name</label><br>
+          <input type="text" name="name" class="form-control" value="" placeholder="Name here...">
+        </div>
+        <div class="form-group">
+          <label for="studNum">Student #</label><br>
+          <input type="text" name="studNum" class="form-control" value="" placeholder="Student # here...">
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label><br>
+          <input type="text" name="email" class="form-control" value="" placeholder="Email here...">
+        </div>
+        <div class="form-group">
+          <label for="article">File input</label>
+          <input type="file" name="article">
+          <p class="help-block">only .doc, .docx, .pdf</p>
+        </div>
+        <div class="form-group">
+          <label for="message">Description</label><br>
+          <textarea name="message" class="form-control" rows="10" cols="30"></textarea>
+        </div>
+        <button type="button" class="btn btn-success" name="button">Submit</button>
+      </form>
+    </div>
+    <div class="col-md-6">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title"><span class="fa fa-certificate"></span> Qualifications</h3>
+        </div>
+        <div class="panel-body">
+          <i class="fa fa-check text-success"> YES</i>
+          <ul>
+            <li>Rule 1</li>
+            <li>Rule 2</li>
+            <li>Rule 3</li>
+            <li>Rule 4</li>
+            <li>Rule 5</li>
+          </ul>
+
+          <hr>
+
+          <i class="fa fa-warning text-danger"> NO</i>
+          <ul>
+            <li>Rule 1</li>
+            <li>Rule 2</li>
+            <li>Rule 3</li>
+            <li>Rule 4</li>
+            <li>Rule 5</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
