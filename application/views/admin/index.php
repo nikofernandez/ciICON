a70ac806fb056055e472cd558ae0ebe6be40d5f0
Oari Kirian <versease101@gmail.com>
2017-06-12 11:47:37 +0800

    Deleted few files and added database configuration

--- application/views/admin/index.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-  <div class="row">
-    <div class="col-md-4 col-md-offset-4">
-      <h1>Hello</h1>
-    </div>
-  </div>
-</div>
---
+++ application/views/news/index.php
@@ -0,0 +1,24 @@
+<div class="container-fluid">
+  <h2><span class="fa fa-bell"></span> Check Out The Latest News <small>Lorem ipsum dolor sit amet</small></h2>
+</div>
+
+<hr>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+
+      <div class="list-group text-center">
+        <?php foreach($categories as $cat) : ?>
+          <a href="<?php echo site_url('/posts/index/'.$cat['cat_id']); ?>" class="list-group-item">
+            <h4 class="list-group-item-heading">
+              <span class="<?php echo $cat['symbol']; ?>"></span>
+              <?php echo $cat['name']; ?>
+            </h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </a>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  </div>
+</div>
