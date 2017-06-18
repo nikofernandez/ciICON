d5d8999131c0e798533ce87af386c83290d19051
Oari Kirian <versease101@gmail.com>
2017-06-18 09:45:15 +0800

    Took out comments

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -29,43 +29,6 @@
             </select>
           </div>
         </div>
-        <!-- <div class="form-group">
-          <label class="col-lg-2 control-label">Category</label>
-          <div class="col-lg-10">
-            <div class="radio">
-              <label>
-                <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
-                Option one is this
-              </label>
-            </div>
-            <div class="radio">
-              <label>
-                <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
-                Option two can be something else
-              </label>
-            </div>
-          </div>
-        </div> -->
-        <!-- <div class="form-group">
-          <label for="select" class="col-lg-2 control-label">Selects</label>
-          <div class="col-lg-10">
-            <select class="form-control" id="select">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-            <br>
-            <select multiple="" class="form-control">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
-            </select>
-          </div>
-        </div> -->
         <div class="form-group">
           <div class="col-lg-10 col-lg-offset-2">
             <button type="submit" class="btn btn-success">Submit</button>
