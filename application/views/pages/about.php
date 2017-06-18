704f189185d94973842339304062394df4e659e7
Oari Kirian <versease101@gmail.com>
2017-06-18 18:25:23 +0800

    Deleted Media Headings

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -2,50 +2,6 @@
   <h1>ICON is Back</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
 </div>
-<div class="container-fluid">
-
-  <div class="row">
-
-    <div class="media boxes">
-
-      <div class="col-md-6">
-        <div class="box well">
-          <div class="media-left media-middle">
-            <a href="#">
-              <h1><span class="fa fa-users media-object"></span></h1>
-            </a>
-          </div>
-          <div class="media-body">
-            <h4 class="media-heading">Media heading</h4>
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-        </div>
-      </div>
-
-    </div> <!-- /media boxes -->
-  </div> <!-- /row -->
-  <div class="row">
-
-    <div class="media boxes">
-
-      <div class="col-md-6 pull-right">
-        <div class="box well">
-          <div class="media-body">
-            <h4 class="media-heading">Media heading</h4>
-            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div>
-          <div class="media-right media-middle">
-            <a href="#">
-              <h1><span class="fa fa-briefcase media-object"></span></h1>
-            </a>
-          </div>
-        </div>
-      </div>
-
-    </div> <!-- /media boxes -->
-  </div> <!-- /row -->
-
-</div> <!-- /container-fluid -->
 
 <div class="container-fluid">
   <div class="page-header">
