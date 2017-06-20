545c32f9f2d60c22b5a6ed68080906885d26fa56
Oari Kirian <versease101@gmail.com>
2017-06-20 19:47:10 +0800

    redirect to news index page

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -31,7 +31,7 @@
           $this->session->set_userdata($admin_data);
 
           $this->session->set_flashdata('admin_loggin_success', 'Welcome Back! :D');
-          redirect('pages/home');
+          redirect('news/index');
 
         }else{
           $this->session->set_flashdata('admin_loggin_failed', 'Login Failed! :(');
