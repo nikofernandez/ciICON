a6d9716ec7f4c217b823ae12433108bd779bc121
Oari Kirian <versease101@gmail.com>
2017-06-20 19:57:15 +0800

    fixed route to login page

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['admin/login'] = 'admin/login';
+$route['admin'] = 'admin/login';
 
 $route['submissions/article'] = 'submissions/article';
 $route['submissions/photo'] = 'submissions/photo';
