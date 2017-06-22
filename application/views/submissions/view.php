e16b33ef4b38942ec5839883b465e62f73b7097b
Oari Kirian <versease101@gmail.com>
2017-06-22 09:05:52 +0800

    Rearrange links

--- application/views/submissions/view.php
+++ application/views/submissions/view.php
@@ -4,27 +4,27 @@
       <div class="row">
         <div class="col-md-4">
           <div class="box">
-            <a id="box-link" href="<?php echo site_url('submissions/alistings'); ?>">
+            <a href="#">
               <i class="fa fa-newspaper-o" aria-hidden="true"></i>
-              <h3>Articles</h3>
+              <h3><a href="<?php echo site_url('submissions/alistings'); ?>">Articles</a></h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
             </a>
           </div>
         </div>
         <div class="col-md-4">
           <div class="box">
-            <a id="box-link" href="<?php echo site_url('submissions/plistings'); ?>">
+            <a href="#">
               <i class="fa fa-photo" aria-hidden="true"></i>
-              <h3>Photos</h3>
+              <h3><a href="<?php echo site_url('submissions/plistings'); ?>">Photos</a></h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
             </a>
           </div>
         </div>
         <div class="col-md-4">
           <div class="box">
-            <a id="box-link" href="<?php echo site_url('submissions/tlistings'); ?>">
+            <a href="#">
               <i class="fa fa-users" aria-hidden="true"></i>
-              <h3>Recruitment</h3>
+              <h3><a href="<?php echo site_url('submissions/tlistings'); ?>">Recruitment</a></h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
             </a>
           </div>
