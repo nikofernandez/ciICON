faea40f33db24870d04e16130069b062900f038f
Oari Kirian <versease101@gmail.com>
2017-06-23 22:59:50 +0800

    Fix issue in css. Change #latest-news to #news-listings and change to container-fluid

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,12 +1,12 @@
-<div class="container">
+<div class="container-fluid">
   <div class="row">
     <div class="col-md-12">
 
       <div class="row" id="latest-news">
         <?php foreach($posts as $post) : ?>
-          <div class="row" id="latest-news">
+          <div class="row" id="news-listings">
             <div class="col-md-3">
-              <img src="http://via.placeholder.com/500x500" class="img img-responsive" alt="">
+              <img src="http://via.placeholder.com/350x200" class="img img-responsive img-thumbnail" alt="">
             </div>
             <div class="col-md-9">
               <h2><?php echo $post['title']; ?> <small>Posted on: <?php echo $post['created_at']; ?></small></h2>
