3d448b3423911587b84db515503fc012dbfca6e9
Oari Kirian <versease101@gmail.com>
2017-06-26 17:03:55 +0800

    Change the view and added banner and side nav

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -5,32 +5,45 @@
   </div>
 </div>
 
-<hr>
-
-<div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="row hidden-md hidden-lg"><h1 class="text-center" >TITULO LARGO DE UNA INVESTIGACION cualquiera</h1></div>
-
-      <div class="pull-left col-md-4 col-xs-12 thumb-contenido">
-        <img class="center-block img-responsive" src='<?php echo base_url(); ?>assets/img/<?php echo $post['photo_name']; ?>' width="500" height="500" />
+<section class="banner-section">
+</section>
+<section class="post-content-section">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12 post-title-block">
+        <h1 class="text-center"><?php echo $post['title']; ?></h1>
+        <ul class="list-inline text-center">
+          <li><?php echo $post['created_at']; ?></li>
+        </ul>
       </div>
-      <div class="">
-        <h1  class="hidden-xs hidden-sm"><?php echo $post['title']; ?></h1>
-        <hr>
-        <small><?php echo $post['created_at']; ?></small>
-        <hr>
+      <div class="col-lg-9 col-md-9 col-sm-12">
+        <div class="image-block">
+          <img src="<?php echo base_url(); ?>assets/img/<?php echo $post['photo_name']; ?>" class="img-responsive" width="550" height="500">
+        </div>
         <p><?php echo $post['body'] ?></p>
       </div>
-   </div>
-
-    <?php if($this->session->userdata('logged_in')) : ?>
-      <div class="col-md-2">
-        <a href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>" class=" btn btn-info btn-sm pull-right"><span class="fa fa-pencil"></span> Edit</a>
-        <?php echo form_open('/posts/delete/'. $post['slug'] ); ?>
-          <button type="submit" class="btn btn-danger btn-sm" name="Delete"><span class="fa fa-times"></span> Delete</button>
-        </form>
+      <div class="col-lg-3  col-md-3 col-sm-12">
+        <div class="well">
+          <h2>Author</h2>
+          <p><?php echo $post['author']; ?></p>
+        </div>
+        <div class="list-group">
+          <a class="list-group-item" href="<?php echo base_url(); ?>submissions/article">
+            <h4 class="list-group-item-heading"><span class="fa fa-newspaper-o"></span> Submit an Article</h4>
+          </a>
+          <a class="list-group-item" href="<?php echo base_url(); ?>submissions/photo">
+            <h4 class="list-group-item-heading"><span class="fa fa-photo"></span> Submit a Photo</h4>
+          </a>
+        </div>
       </div>
-    <?php endif; ?>
-  </div>
-</div>
+      <?php if($this->session->userdata('logged_in')) : ?>
+        <div class="col-md-2">
+          <a href="<?php echo base_url(); ?>posts/edit/<?php echo $post['slug']; ?>" class=" btn btn-info btn-sm pull-right"><span class="fa fa-pencil"></span> Edit</a>
+          <?php echo form_open('/posts/delete/'. $post['slug'] ); ?>
+            <button type="submit" class="btn btn-danger btn-sm" name="Delete"><span class="fa fa-times"></span> Delete</button>
+          </form>
+        </div>
+      <?php endif; ?>
+    </div>
+  </div> <!-- /container -->
+</section>
