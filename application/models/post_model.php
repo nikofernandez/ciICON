dbbc0eb74a179232bb9b7976d462ff02862d232b
Oari Kirian <versease101@gmail.com>
2017-06-30 18:11:37 +0800

    FIXED BUG: latest news error showed all w/ no limit

--- application/models/post_model.php
+++ application/models/post_model.php
@@ -4,7 +4,10 @@
       $this->load->database(); // Loads database
     }
 
-    public function get_posts($slug = FALSE){
+    public function get_posts($slug = FALSE, $limit = FALSE, $offset = FALSE){
+      if ($limit) { // Set results limit w/ offset 0
+        $this->db->limit($limit, $offset);
+      }
 
       if($slug === FALSE){
         $this->db->order_by('news.id', 'DESC'); // Outputs the data by ID in descending order
