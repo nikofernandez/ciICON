af65138aad841a6973157afec3998f1ccce8fe32
Oari Kirian <versease101@gmail.com>
2017-06-30 08:54:04 +0800

    Finished "Meet the Team" section. [Incomplete image]

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,9 +1,6 @@
 
 
 <div class="container-fluid">
-  <div class="page-header">
-    <h1><span class="fa fa-history"></span> History</h1>
-  </div>
 
   <ul class="timeline">
     <li>
@@ -61,7 +58,7 @@
   <?php // TODO: Add "Meet the Team" section ?>
   <section id="team-list">
     <div class="row">
-      <div class="col-md-4">
+      <div class="col-md-6">
         <img src="https://scontent-hkg3-1.xx.fbcdn.net/v/t34.0-12/18788664_1885202465084356_1812123590_n.jpg?oh=e2695cc33b32d81aff40a368a1221404&oe=5956E9BB"
         class="img-team center-block" >
         <h3 class="text-center">
@@ -69,7 +66,7 @@
           <small>Editor-in-Chief</small>
         </h3>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-6">
         <img src="https://scontent-hkg3-1.xx.fbcdn.net/v/t34.0-12/18902318_1885232411748028_726358551_n.jpg?oh=be66279ba1b242245c8d474fdd0afe6c&oe=5957468E"
         class="center-block img-team" >
         <h3 class="text-center">
@@ -86,7 +83,7 @@
         </h3>
       </div>
       <div class="col-md-4">
-        <img src="https://scontent-hkg3-1.xx.fbcdn.net/v/t1.0-9/18194978_10209349051858814_6386714908875355995_n.jpg?oh=23a41042a5e0b69f4ec864579d7990e2&oe=59CF8F9A"
+        <img src="https://scontent.fmnl3-2.fna.fbcdn.net/v/t34.0-12/18600777_10211211519357101_925333358_n.jpg?oh=6ff6bede97f1528ca9e3abf14235e764&oe=5957A30C"
         class="img-team center-block" >
         <h3 class="text-center">
           Elniley Manansala<br>
@@ -94,20 +91,102 @@
         </h3>
       </div>
       <div class="col-md-4">
-        <img src="https://scontent-hkg3-1.xx.fbcdn.net/v/t1.0-9/12647497_1670642946540310_2942286637955321391_n.jpg?oh=cc56928bbf9929a5b5a1a1c4659102b6&oe=59D0DD2D"
+        <img src="https://scontent.fmnl3-2.fna.fbcdn.net/v/t34.0-12/18600777_10211211519357101_925333358_n.jpg?oh=6ff6bede97f1528ca9e3abf14235e764&oe=5957A30C"
         class="center-block img-team" >
         <h3 class="text-center">
           Maki Soneja<br>
           <small>Digitals Director</small>
         </h3>
       </div>
-      <div class="col-md-4">
+    </div>
+    <div class="page-header team-name">
+      <h1 class="text-center">Digitals</h1>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
         <img src="https://scontent-hkg3-1.xx.fbcdn.net/v/t34.0-12/18928406_1885232328414703_905670425_n.jpg?oh=d80ddcebf274dec1ffa72e8f1494c28b&oe=5956FC3A"
         class="center-block img-team">
         <h3 class="text-center">
           Jeck Tana<br>
           <small>Layout Artist</small>
         </h3>
+      </div>
+      <div class="col-md-6">
+        <img src="https://scontent-hkg3-1.xx.fbcdn.net/v/t34.0-12/18871193_1885172355087367_404657283_n.jpg?oh=5db46c847f9814b4b4f118de51769ed1&oe=59576DCD"
+        class="img-team center-block" >
+        <h3 class="text-center">
+          Winson Langcay<br>
+          <small>Photographer</small>
+        </h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="page-header team-name">
+        <h1 class="text-center">Editors</h1>
+      </div>
+      <div class="col-md-4">
+        <img src="https://scontent-hkg3-1.xx.fbcdn.net/v/t34.0-12/18945103_1885229711748298_54562823_n.jpg?oh=52170434b28b019ff2ed1878ff2694cd&oe=5957676F"
+        class="center-block img-team" >
+        <h3 class="text-center">
+          Michelle Lu<br>
+          <small>Copy Editor</small>
+        </h3>
+      </div>
+      <div class="col-md-4">
+        <img src="https://scontent.fmnl3-2.fna.fbcdn.net/v/t34.0-12/18600777_10211211519357101_925333358_n.jpg?oh=6ff6bede97f1528ca9e3abf14235e764&oe=5957A30C"
+        class="center-block img-team">
+        <h3 class="text-center">
+          Honeysett Jaula<br>
+          <small>Literary Editor</small>
+        </h3>
+      </div>
+      <div class="col-md-4">
+        <img src="https://scontent-hkg3-1.xx.fbcdn.net/v/t34.0-12/18902352_1885172248420711_1773907923_n.jpg?oh=6e668f2140881c3ec20ef7866f196bb7&oe=595832F7"
+        class="img-team center-block" >
+        <h3 class="text-center">
+          Zap Tablizo<br>
+          <small>Sports Editor</small>
+        </h3>
+      </div>
+      <div class="col-md-3">
+        <img src="https://scontent-hkg3-1.xx.fbcdn.net/v/t34.0-12/18835275_1885229668414969_1372955251_n.jpg?oh=21a70aac6a680edf7e9f14218d420e8f&oe=59576A48"
+        class="center-block img-team" >
+        <h3 class="text-center">
+          Glen Castro<br>
+          <small>Technology Editor</small>
+        </h3>
+      </div>
+      <div class="col-md-3">
+        <img src="https://scontent.fmnl3-2.fna.fbcdn.net/v/t34.0-12/18902805_1885172225087380_1178514567_n.jpg?oh=0d6d421fdd079c92a14f220c6880546c&oe=5957C170"
+        class="center-block img-team">
+        <h3 class="text-center">
+          Ruth Alexis Cruz<br>
+          <small>Business Editor</small>
+        </h3>
+      </div>
+      <div class="col-md-3">
+        <img src="https://scontent.fmnl3-2.fna.fbcdn.net/v/t34.0-12/18600777_10211211519357101_925333358_n.jpg?oh=6ff6bede97f1528ca9e3abf14235e764&oe=5957A30C"
+        class="center-block img-team">
+        <h3 class="text-center">
+          Jasmin<br>
+          <small>Feature Editor</small>
+        </h3>
+      </div>
+      <div class="col-md-3">
+        <img src="https://scontent.fmnl3-2.fna.fbcdn.net/v/t34.0-12/18600777_10211211519357101_925333358_n.jpg?oh=6ff6bede97f1528ca9e3abf14235e764&oe=5957A30C"
+        class="center-block img-team">
+        <h3 class="text-center">
+          Jaeztine<br>
+          <small>Campus News Editor</small>
+        </h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="page-header team-name">
+        <h1 class="text-center">News Correspondent</h1>
+      </div>
+      <div class="col-md-12">
+        <img src="http://placehold.it/1400x500/16a085/text=News Correspondent" alt="News Correspondent" class="center-block news-corr img-responsive">
       </div>
     </div>
   </section>
